9696c392be8a6986d18d367ed78a76f0608053c9
simonbucko <38184399+simonbucko@users.noreply.github.com>
2020-04-30 23:46:20 +0200

    ba

--- js/typing.js
+++ js/typing.js
@@ -4,12 +4,24 @@ let index = 0;
 let currentText = '';
 let letter = '';
 let letterBackWard = '';
+let textField = document.querySelector('#profilTextBox');
 
 function pause(duration) {
     let time = new Date().getTime();
     let currentTime = 0;
     while (currentTime - time < duration) {
         currentTime = new Date().getTime();
+    }
+}
+
+function deleting(letter, length) {
+    pause(3000);
+    for (let i = 1; i < length + 1; i++) {
+        letter = currentText.slice(0, -i);
+        pause(400);
+        // console.log(letter);
+        textField.textContent = letter;
+        console.log(textField.textContent);
     }
 }
 
@@ -21,18 +33,20 @@ function pause(duration) {
     //writting letters
     currentText = texts[count];
     letter = currentText.slice(0, ++index);
-    document.querySelector('#profilTextBox').textContent = letter;
+    textField.textContent = letter;
 
     //deleting letters
     if (letter.length === currentText.length) {
-        pause(3000);
-        let indexBackward = letter.length;
-        for (let i = 0; i < indexBackward; i++) {
-            letter = letter.slice(0, -1);
-            pause(400);
-            console.log(letter);
-            document.querySelector('#profilTextBox').textContent = letter;
-        }
+        deleting(letter, letter.length);
+
+        // pause(3000);
+        // let indexBackward = letter.length;
+        // for (let i = 0; i < indexBackward; i++) {
+        //     letter = letter.slice(0, -1);
+        //     pause(400);
+        //     console.log(letter);
+        //     textField.textContent = letter;
+        // }
         count++;
         index = 0;
     }
