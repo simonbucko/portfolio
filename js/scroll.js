9346c6abb99593854a2a4a0ee992c023d4306738
simonbucko <38184399+simonbucko@users.noreply.github.com>
2020-05-02 14:04:44 +0200

    bla

--- js/scroll.js
+++ js/scroll.js
@@ -1,9 +1,8 @@
 const buttonDown = document.getElementById('down');
 const buttonMiddle = document.getElementById('middle');
 const buttonUp = document.getElementById('up');
-const buttonFooter = document.getElementById('footer');
 const sections = document.querySelectorAll('section');
-let buttons = [buttonUp, buttonMiddle, buttonDown, buttonFooter];
+let buttons = [buttonUp, buttonMiddle, buttonDown];
 
 function activeButtons(buttonsLocal, index) {
     buttonsLocal.forEach((button, i) => {
@@ -16,19 +15,10 @@ function activeButtons(buttonsLocal, index) {
 activeButtons(buttons, 0);
 
 //Buttons clicks scrolling
-buttonFooter.addEventListener('click', () => {
-    sections[3].scrollIntoView({ behavior: "smooth" });
-})
-buttonDown.addEventListener('click', () => {
-    sections[2].scrollIntoView({ behavior: "smooth" });
-})
-
-buttonMiddle.addEventListener('click', () => {
-    sections[1].scrollIntoView({ behavior: "smooth" });
-})
-
-buttonUp.addEventListener('click', () => {
-    sections[0].scrollIntoView({ behavior: "smooth" });
+buttons.forEach((button, i) => {
+    button.addEventListener('click', () => {
+        sections[i].scrollIntoView({ behavior: "smooth" });
+    })
 })
 //Scrolling onClick
 sections.forEach(section => {
@@ -53,9 +43,9 @@ sections.forEach(section => {
 window.addEventListener('scroll', () => {
     let firstElementMiddle = (sections[0].offsetTop + sections[1].offsetTop) / 2;
     let secondElementMiddle = (sections[1].offsetTop + sections[2].offsetTop) / 2;
-    let thirdElementMiddle = (sections[2].offsetTop + sections[3].offsetTop) / 2;
+    // let thirdElementMiddle = (sections[2].offsetTop + sections[3].offsetTop) / 2;
     if (window.pageYOffset <= firstElementMiddle) activeButtons(buttons, 0);
     else if (window.pageYOffset > firstElementMiddle && window.pageYOffset < secondElementMiddle) activeButtons(buttons, 1);
-    else if (window.pageYOffset > secondElementMiddle && window.pageYOffset < thirdElementMiddle) activeButtons(buttons, 2);
-    else activeButtons(buttons, 3);
+    // else if (window.pageYOffset > secondElementMiddle && window.pageYOffset < thirdElementMiddle) activeButtons(buttons, 2);
+    else activeButtons(buttons, 2);
 })
